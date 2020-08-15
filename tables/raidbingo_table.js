9b79e47a56975f33125958a9f51cd13760026cfc
lukeprism <64792304+lukeprism@users.noreply.github.com>
2020-08-15 13:44:14 -0400

    tble v0.257 or smth idk
    a couple more memes and some updates to both probability and relevancy. lets make some moolah

--- tables/raidbingo_table.js
+++ tables/raidbingo_table.js
@@ -35,8 +35,10 @@ bingoList[5] = [
   { name: "Pull at 3 mins before lockout", types: ["statement"] },
   { name: "someone's gear breaks", types: ["someone"] },
   { name: "Sam jumps off the arena", types: ["sam","jspecific"] },
+  { name: "Rito jumps off the arena", types: ["rito","jspecific"] },
   { name: "robot has entered the voice chat", types: ["pspecific","statement","meta"] },
-  { name: "'only >100% durability?! gee whiz'", types: ["pspecific","quote"] }
+  { name: "'only >100% durability?! gee whiz'", types: ["pspecific","quote"] },
+  { name: "Judy makes a diagram", types: ["judy"] }
 ];
 bingoList[6] = [
   { name: "vintage meme discussion", types: ["pspecific","statement","meta"] },
@@ -50,16 +52,15 @@ bingoList[6] = [
 bingoList[7] = [
   { name: "'sorry i didn't have bene'", types: ["quote","jspecific","heals"] },
   { name: "the gang is cleaved", types: ["statement"] },
-  { name: "'so you *can* die by falling off'", types: ["expeltive","quote"] },
   { name: "*clack clack clack*", types: ["noise","pspecific"] },
-  { name: "'this is just like [past raid]!!!'", types: ["quote","canon"] },
+  { name: "'this is just like [past raid/mech]!!!'", types: ["quote","canon"] },
   { name: "'why do i have aggro'", types: ["quote"] }
 ];
 bingoList[8] = [
-  { name: "'Marisa stance?'", types: ["marisa","quote","jspecific"] },
+  { name: "'[tank] stance?'", types: ["quote","jspecific"] },
   { name: "someone announces they'll brb right when someone returns from brb", types: ["someone","meta"] },
   { name: "healers aggressively claiming their cohealer did all the work", types: ["statement","heals"] },
-  { name: "Marisa makes some pots", types: ["marisa"] },
+  { name: "Marisa makes some food", types: ["marisa"] },
   { name: "someone swaps glams between lockouts", types: ["someone"] },
   { name: "irresponsible use of rescue", types: ["statement","heals"] }
 ];
@@ -69,7 +70,7 @@ bingoList[9] = [
   { name: "(se.6)", types: ["noise","emote"] },
   { name: "'stack on his butt'", types: ["quote","mech"] },
   { name: "gobbiespeak", types: ["statement"] },
-  { name: "someone draws a diagram", types: ["someone","meta"] },
+  { name: "someone links a diagram", types: ["someone","meta"] },
   { name: "Janelle", types: ["expeltive","pspecific"] }
 ];
 bingoList[10] = [
@@ -86,7 +87,7 @@ bingoList[11] = [
   { name: "calls the mechanic out right after it happens", types: ["mech","statement"] },
   { name: "'Sam, Megabeam?' when the attack is clearly not Megabeam", types: ["sam","mech"] },
   { name: "'go to L' (i is also acceptable)", types: ["mech","quote"] },
-  { name: "Aeo fails to tankswap", types: ["aeo","meta"] },
+  { name: "Ellory calls something sexy", types: ["ellory","meta"] },
   { name: "someone references the bingo card (you are disqualified)", types: ["meta"] }
 ];
 bingoList[12] = [
@@ -100,23 +101,27 @@ bingoList[13] = [
   { name: "someone mentions t6s and everyone wails", types: ["canon","someone"] },
   { name: "someone starts singing", types: ["someone","meta","noise"] },
   { name: "forgets to unmute themselves for five minutes", types: ["meta"] },
+  { name: "'sorry my cat/dog/bird is vomiting'", types: ["quote"] },
   { name: "something cursed is linked in vc chat", types: ["meta","statement"] }
 ];
 bingoList[14] = [
   { name: "someone brb comes back to find themselves dead and the party whistling at spawn", types: ["meta","someone"] },
-  { name: "/yoldance", types: ["emote"] },
   { name: "Aeo is referred to as Rito and only Aeo notices", types: ["aeo","statement"] },
-  { name: "Alexander nostalgia", types: ["statement","canon"] }
+  { name: "Alexander nostalgia", types: ["statement","canon"] },
+  { name: "guys lets just do uwu instead", types: ["meta"] },
+  { name: "someone who is definitely not playing a tank role is shirked", types: ["meme","jspecific"] }
 ];
 bingoList[15] = [
   { name: "a semi-coherent conversation is had using exclusively the autotranslate function", types: ["statement"] },
   { name: "someone learns a new word and wishes they hadn't", types: ["someone","meta"] },
   { name: "someone forgets to switch classes before queue and rolls with it", types: ["someone","meta"] },
   { name: "/tdance", types: ["emote"] },
-  { name: "/gdance", types: ["emote"] }
+  { name: "/gdance", types: ["emote"] },
+  { name: "/yoldance", types: ["emote"] }
 ];
 bingoList[16] = [
  { name: "someone accidentally walks off the arena", types: ["someone","dspecific"] },
+ { name: "'so you *can* die by falling off'", types: ["expeltive","quote"] },
  { name: ">duty actions", types: ["mech","dspecific"] },
  { name: "someone jumps off on purpose to accelerate the wipe", types: ["mech","statement"] },
  { name: "unfortunatly timed acceptance of raise results in instant death", types: ["mech","statement"] },
@@ -125,34 +130,36 @@ bingoList[16] = [
 bingoList[17] = [
  { name: "committing to an incorrect callout just to save face", types: ["mech","statement"] },
  { name: "'ENOCHAN NO'", types: ["expeltive","jspecific"] },
-  { name: "'oops i got greedy'", types: ["quote"] },
- { name: "someone accidentally facepulls the boss", types: ["someone"] }
+ { name: "'oops i got greedy'", types: ["quote"] },
+ { name: "someone accidentally facepulls the boss", types: ["someone"] },
+ { name: "someone abuses a safety pixel", types: ["someone"] }
 ];
 bingoList[18] = [
  { name: "the raise misses and no one else has swift", types: ["jspecific"] },
  { name: "'wow that did [my max health] in damage'", types: ["quote"] },
  { name: "an attack does a frankly absurd amount of damage", types: ["statement"] },
- { name: "pokeball of death results in a d/c mid-pull", types: ["pspecific","statement"] }
+ { name: "pokeball of death results in a d/c mid-pull", types: ["pspecific","statement"] },
+ { name: "internet problems :tm:", types: ["quote"] }
 ];
 bingoList[19] = [
  { name: "late start due to timezone shenanigans", types: ["statement"] },
  { name: "'we have to clear before maintenance tonight'", types: ["special","quote"] },
  { name: "spinning around in place prepull to assert dominance", types: ["statement"] },
- { name: "someone is late and blames it on the queue", types: ["someone"] }
+ { name: "someone is late and blames it on the queue", types: ["someone"] },
+ { name: "someone nearly spoils MSQ for a slacker", types: ["someone"] }
 ];
 bingoList[20] = [
  { name: "Elidibus Serpentarius comes up", types: ["meta","canon"] },
  { name: "'social distancing ya'll lets spread'", types: ["meta","mech","quote"] },
  { name: "'that looks like a tankswap'", types: ["mech","quote"] },
- { name: "'hold on my cat is puking'", types: ["quote","meta"] },
- { name: "forward and back and fooorward and back and", types: ["noise","canon"] }
+ { name: "forward and back and fooorward and back and", types: ["noise","canon"] },
+ { name: "marker placement is berated", types: ["rito","jspecific"] }
 ];
 bingoList[21] = [
  { name: "everyone screaming for LB when it's already casting", types: ["lb","statement","special"] },
- { name: "ps4 load times", types: ["sam","meta"] },
  { name: "'you don't pay my sub'", types: ["quote","meta"] },
  { name: "'wait don't queue i need to repair'", types: ["quote"] },
- { name: "Rito jumps off the arena", types: ["rito","jspecific"] }
+ { name: "healer lb3 cheeseing a mech", types: ["mech","lb"] }
 ];
 bingoList[22] = [
  { name: "Complete silence when it's [The Pull]", types: ["statement","special"] },
@@ -172,6 +179,7 @@ bingoList[24] = [
  { name: "controller woes", types: ["meta","pspecific"] },
  { name: "mouse woes", types: ["meta"] },
  { name: "'you are the bear'", types: ["meta","canon","mech"] },
+ { name: "the gang's all here (no subs)", types: ["meta"] },
  { name: "going over mechs for a sub", types: ["mech","sub"] }
 ];
 bingoList[25] = [
