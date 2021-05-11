51ed01c19df4acfdeb18d96ad173ea863fb22be0
lukeprism <64792304+lukeprism@users.noreply.github.com>
2021-05-10 20:42:26 -0400

    v1.000002
    fanfest bing test

---
+++ tables/xivbingo_table.js
@@ -0,0 +1,139 @@
+<!-- hey. no cheating. -->
+var bingoList = [];
+bingoList[1] = [
+  { name: "wacky yoship hijinx", types: ["expeltive"] },
+  { name: "event started late", types: ["question","quote"] },
+  { name: "wacky kojifox hijinx", types: ["statement"] },
+  { name: "audio issues", types: ["quote"] }
+];
+bingoList[2] = [
+  { name: "parsing is mentioned", types: ["quote"] },
+  { name: "another ff game is mentioned by name", types: ["someone"] },
+  { name: "belt joke", types: ["lb","statement"] },
+  { name: "TO THE MOON", types: ["question","calvin"] }
+];
+bingoList[3] = [
+  { name: "female hroth and/or male viera", types: ["marisa","ce"] },
+  { name: "no femhroth or bunnyboys but it is addressed", types: ["marisa","ce"] },
+  { name: "'and now a word from our sponsors'", types: ["rito","statement"] },
+  { name: "'did u see the merch?? did you???'", types: ["judy","statement"] }
+];
+bingoList[4] = [
+  { name: "Oops! All FF4 references", types: ["quote"] },
+  { name: "'the japanese name is way cooler'", types: ["mech"] },
+  { name: "FF7 crossover/tie-in", types: ["lyra","emote"] },
+  { name: "Chrono Trigger reference", types: ["expeltive","question"] }
+];
+bingoList[5] = [
+  { name: "gear squish", types: ["statement"] },
+  { name: "number squish = ARR-esque numbers", types: ["someone"] },
+  { name: "noct ast is shot live on stream", types: ["sam","jspecific"] },
+  { name: "Judy makes a diagram", types: ["judy"] }
+];
+bingoList[6] = [
+  { name: "more gender unlocked clothing", types: ["pspecific","statement","meta"] },
+  { name: "hroth/viera hat compatibility", types: ["mech"] },
+  { name: "more glamour plate slots", types: ["mech"] }
+];
+bingoList[7] = [
+  { name: "Primals play ancient eggroll come ready to fry", types: ["quote","jspecific","heals"] },
+  { name: "Primals play Battle on the Big Bridge", types: ["statement"] },
+  { name: "Primals feat. kojifox on vocals", types: ["noise","pspecific"] },
+  { name: "Primals advertise their cover album", types: ["quote"] }
+];
+bingoList[8] = [
+  { name: "new dps is Reaper", types: ["quote","jspecific"] },
+  { name: "new dps is Assassin", types: ["someone","meta"] },
+  { name: "new dps uses a scythe and is very cool and pog", types: ["statement","heals"] },
+  { name: "Zenos is the new dps mascot", types: ["statement","heals"] }
+];
+bingoList[9] = [
+  { name: "new lore is dropped apropos of nothing", types: ["mech","statement"] },
+  { name: "namazu lore", types: ["jspecific","sattement"] },
+  { name: "hints that we will be murdering Hydaelyn", types: ["someone","meta"] },
+];
+bingoList[10] = [
+  { name: "spaghetti code is referenced", types: ["jspecific","someone"] },
+  { name: "new art/locations shown in a panel when half of NA is asleep", types: ["gquote","statement","canon"] },
+  { name: "chat throws a presenter for a loop", types: ["sam","statement"] },
+  { name: "new yoshida art", types: ["mech","statement"] }
+];
+bingoList[11] = [
+  { name: "they got rid of aether currents", types: ["pspecific","mech"] },
+  { name: "tataru becomes a trust member", types: ["mech","statement"] },
+  { name: "more housing wards added", types: ["sam","mech"] },
+  { name: "number of housing items alloted increased", types: ["mech","quote"] },
+];
+bingoList[12] = [
+  { name: "we're going to sharlyan", types: ["mech","quote"] },
+  { name: "moon beast tribe announced", types: ["expeltive","quote"] },
+  { name: "moon zone is teased", types: ["mech","lyra"] },
+  { name: "new important character is an unplayable race", types: ["mech"] }
+];
+bingoList[13] = [
+  { name: "we get full ver. of endwalker theme and its a BOP", types: ["canon","someone"] },
+  { name: "vocalist on stage", types: ["someone","meta","noise"] },
+  { name: "new music played and it's dope", types: ["meta"] },
+  { name: "new PVP mode announced", types: ["meta","statement"] }
+];
+bingoList[14] = [
+  { name: "new method of transportation", types: ["meta","someone"] },
+  { name: "Aeo is referred to as Rito and only Aeo notices", types: ["aeo","statement"] },
+  { name: "someone who is definitely not playing a tank role is shirked", types: ["meme","jspecific"] }
+];
+bingoList[15] = [
+  { name: "a semi-coherent conversation is had using exclusively the autotranslate function", types: ["statement"] },
+  { name: "/gdance", types: ["emote"] },
+  { name: "/yoldance", types: ["emote"] }
+];
+bingoList[16] = [
+ { name: "someone accidentally walks off the arena", types: ["someone","dspecific"] },
+ { name: "unfortunatly timed acceptance of raise results in instant death", types: ["mech","statement"] },
+ { name: "healer has dance partner for some reason", types: ["jspecific"] }
+];
+bingoList[17] = [
+ { name: "committing to an incorrect callout just to save face", types: ["mech","statement"] },
+ { name: "'ENOCHAN NO'", types: ["expeltive","jspecific"] },
+ { name: "someone abuses a safety pixel", types: ["someone"] }
+];
+bingoList[18] = [
+ { name: "the raise misses and no one else has swift", types: ["jspecific"] },
+ { name: "'wow that did [my max health] in damage'", types: ["quote"] },
+ { name: "internet problems :tm:", types: ["quote"] }
+];
+bingoList[19] = [
+ { name: "late start due to timezone shenanigans", types: ["statement"] },
+ { name: "'we have to clear before maintenance tonight'", types: ["special","quote"] },
+ { name: "someone nearly spoils MSQ for a slacker", types: ["someone"] }
+];
+bingoList[20] = [
+ { name: "Elidibus Serpentarius comes up", types: ["meta","canon"] },
+ { name: "'social distancing ya'll lets spread'", types: ["meta","mech","quote"] },
+ { name: "marker placement is berated", types: ["rito","jspecific"] }
+];
+bingoList[21] = [
+ { name: "everyone screaming for LB when it's already casting", types: ["lb","statement","special"] },
+ { name: "'you don't pay my sub'", types: ["quote","meta"] },
+ { name: "healer lb3 cheeseing a mech", types: ["mech","lb"] }
+];
+bingoList[22] = [
+ { name: "Complete silence when it's [The Pull]", types: ["statement","special"] },
+ { name: "'ah wait, gotta start recording/the stream'", types: ["quote","rito"] },
+ { name: "someone is a different race than they were last session", types: ["someone","special"] }
+];
+bingoList[23] = [
+ { name: "'DON'T LEAVE we need a screenshot'", types: ["statement","quote","special"] },
+ { name: "an executive decision to turn echo back on is made", types: ["meta","special"] },
+ { name: "overheal is 60%", types: ["meta","jspecific"] }
+];
+bingoList[24] = [
+ { name: "there's a sub this session", types: ["sub","special"] },
+ { name: "controller woes", types: ["meta","pspecific"] },
+ { name: "going over mechs for a sub", types: ["mech","sub"] }
+];
+bingoList[25] = [
+  { name: "first clear in 3+ weeks", types: ["special"] },
+  { name: "we got the clear!!", types: ["special"] },
+  { name: "cleared on the first pull", types: ["special"] }
+];
+$(function() { srl.bingo(bingoList, 5); });
